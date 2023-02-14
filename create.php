600b5966907ccc714a024220d1d274c4884e6633
Jean-Claude AZIAHA <aziaha.formations@gmail.com>
2023-02-14 17:02:39 +0100

    Affichage des messages d'erreurs du formulaire

--- create.php
+++ create.php
@@ -53,15 +53,112 @@ session_start();
         $errors = [];
 
         // Pour le prénom
-        if (isset($post_clean['first_name'])) 
+        if (isset($post_clean['first_name']))
         {
             if ( is_blank($post_clean['first_name']) ) 
             {
                 $errors['first_name'] = "Le prénom est obligatoire.";
             }
+            if ( length_is_greater_than($post_clean['first_name'], 255) )
+            {
+                $errors['first_name'] = "Le prénom ne doit pas dépasser 255 caractères.";
+            }
         }
 
-        var_dump($errors); die();
+        // Pour le nom
+        if (isset($post_clean['last_name']))
+        {
+            if ( is_blank($post_clean['last_name']) ) 
+            {
+                $errors['last_name'] = "Le nom est obligatoire.";
+            }
+            if ( length_is_greater_than($post_clean['last_name'], 255) )
+            {
+                $errors['last_name'] = "Le nom ne doit pas dépasser 255 caractères.";
+            }
+        }
+
+        // Pour l'email
+        if (isset($post_clean['email']))
+        {
+            if ( is_blank($post_clean['email']) ) 
+            {
+                $errors['email'] = "L'email est obligatoire.";
+            }
+            else if ( length_is_less_than($post_clean['email'], 5) )
+            {
+                $errors['email'] = "L'email doit contenir au moins 5 caractères.";
+            }
+            else if ( length_is_greater_than($post_clean['email'], 255) )
+            {
+                $errors['email'] = "L'email ne doit pas dépasser 255 caractères.";
+            }
+            else if ( is_invalid_email($post_clean['email']) )
+            {
+                $errors['email'] = "Veuillez entrer un email valide.";
+            }
+            else if ( is_already_exists_on_create($post_clean['email'], "contact", "email") )
+            {
+                $errors['email'] = "Cet email appartient déjà à l'un de vos contacts.";
+            }
+        }
+
+        // var_dump($post_clean['age']); die();
+
+        if (isset($post_clean['age']))
+        {
+            if ( is_not_blank($post_clean['age']) ) 
+            {
+                if ( is_not_a_number($post_clean['age']) )
+                {
+                    $errors['age'] = "L'âge doit être un nombre.";
+                }
+                if ( is_not_between($post_clean['age'], 3, 300) ) 
+                {
+                    $errors['age'] = "L'age doit être compris entre 3 et 300 ans.";
+                }
+            }
+        }
+
+
+
+        if (isset($post_clean['phone']))
+        {
+            if ( is_blank($post_clean['phone']) ) 
+            {
+                $errors['phone'] = "Le numéro de téléphone est obligatoire.";
+            }
+            else if ( is_invalid_phone($post_clean['phone']) ) // Pause
+            {
+                $errors['phone'] = "Veuillez entrer un numéro de téléphone valide.";
+            }
+        }
+
+
+        if (isset($post_clean['comment']))
+        {
+            if ( is_not_blank($post_clean['comment']) ) 
+            {
+                if ( length_is_greater_than($post_clean['comment'], 4000) ) 
+                {
+                    $errors['comment'] = "Le commentaire ne doit pas dépasser 4000 caractères.";
+                }
+            }
+        }
+
+        // Si le tableau contient au moins une erreur
+        if ( count($errors) > 0 ) 
+        {
+            // Sauvegardons les messages d'erreurs en session.
+            $_SESSION['create_form_errors'] = $errors;
+
+            // Sauvegardons les données provenant du formulaire en session.
+            $_SESSION['create_form_old_values'] = $post_clean;
+
+            // Effectuons une redirection vers la page de laquelle proviennent les informations
+            // Puis, arrêtons l'exécution du script.
+            return header("Location: " . $_SERVER["HTTP_REFERER"]);
+        }
 
     }
 
@@ -90,22 +187,35 @@ session_start();
     <main class="container">
         <h1 class="text-center my-3 display-5">Nouveau contact</h1>
 
+        
         <div class="container">
             <div class="row">
                 <div class="col-md-8 col-lg-7 mx-auto p-4 shadow bg-white">
+
+                    <?php if( isset($_SESSION['create_form_errors']) && !empty($_SESSION['create_form_errors']) ) : ?>
+                        <div class="alert alert-danger" role="alert">
+                            <ul>
+                                <?php foreach($_SESSION['create_form_errors'] as $error) : ?>
+                                    <li><?= $error ?></li>
+                                <?php endforeach ?>
+                            </ul>
+                        </div>
+                        <?php unset($_SESSION['create_form_errors']); ?>
+                    <?php endif ?>
+
                     <form method="POST">
 
                         <div class="row">
                             <div class="col md-6">
                                 <div class="mb-3">
                                     <label for="create_form_first_name">Prénom</label>
-                                    <input type="text" name="first_name" id="create_form_first_name" class="form-control">
+                                    <input type="text" name="first_name" id="create_form_first_name" class="form-control" value="<?= isset($_SESSION['create_form_old_values']['first_name']) ? $_SESSION['create_form_old_values']['first_name'] : '' ; unset($_SESSION['create_form_old_values']['first_name']); ?>">
                                 </div>
                             </div>
                             <div class="col md-6">
                                 <div class="mb-3">
                                     <label for="create_form_last_name">Nom</label>
-                                    <input type="text" name="last_name" id="create_form_last_name" class="form-control">
+                                    <input type="text" name="last_name" id="create_form_last_name" class="form-control" value="<?= isset($_SESSION['create_form_old_values']['last_name']) ? $_SESSION['create_form_old_values']['last_name'] : '' ; unset($_SESSION['create_form_old_values']['last_name']); ?>">
                                 </div>
                             </div>
                         </div>
@@ -114,25 +224,25 @@ session_start();
                             <div class="col-md-8">
                                 <div class="mb-3">
                                     <label for="create_form_email">Email</label>
-                                    <input type="email" name="email" id="create_form_email" class="form-control">
+                                    <input type="email" name="email" id="create_form_email" class="form-control" value="<?= isset($_SESSION['create_form_old_values']['email']) ? $_SESSION['create_form_old_values']['email'] : '' ; unset($_SESSION['create_form_old_values']['email']); ?>">
                                 </div>
                             </div>
                             <div class="col-md-4">
                                 <div class="mb-3">
                                     <label for="create_form_age">Age</label>
-                                    <input type="number" name="age" id="create_form_age" class="form-control">
+                                    <input type="number" name="age" id="create_form_age" class="form-control" value="<?= isset($_SESSION['create_form_old_values']['age']) ? $_SESSION['create_form_old_values']['age'] : '' ; unset($_SESSION['create_form_old_values']['age']); ?>">
                                 </div>
                             </div>
                         </div>
 
                         <div class="mb-3">
                             <label for="create_form_phone">Numéro de téléphone</label>
-                            <input type="tel" name="phone" id="create_form_phone" class="form-control">
+                            <input type="tel" name="phone" id="create_form_phone" class="form-control" value="<?= isset($_SESSION['create_form_old_values']['phone']) ? $_SESSION['create_form_old_values']['phone'] : '' ; unset($_SESSION['create_form_old_values']['phone']); ?>">
                         </div>
 
                         <div class="mb-3">
                             <label for="create_form_comment">Commentaires</label>
-                            <textarea name="comment" id="create_form_comment" class="form-control" rows="4"></textarea>
+                            <textarea name="comment" id="create_form_comment" class="form-control" rows="4"><?= isset($_SESSION['create_form_old_values']['comment']) ? $_SESSION['create_form_old_values']['comment'] : '' ; unset($_SESSION['create_form_old_values']['comment']); ?></textarea>
                         </div>
 
                         <div class="mb-3 d-none">
@@ -144,7 +254,7 @@ session_start();
                         </div>
 
                         <div class="mb-3">
-                            <input type="submit" class="btn btn-primary shadow" value="Ajouter">
+                            <input type="submit" class="btn btn-primary shadow" value="Ajouter" formnovalidate>
                         </div>
                     </form>
                 </div>
---
+++ db/connexion.php
@@ -0,0 +1,18 @@
+<?php
+
+    /* Connexion à une base MySQL avec l'invocation de pilote */
+    $dsn_db = 'mysql:dbname=dwwm-agenda;host=127.0.0.1';
+    $user_db = 'root';
+    $password_db = '';
+
+    try 
+    {
+        $db = new PDO($dsn_db, $user_db, $password_db);
+        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    } 
+    catch (PDOException $e)
+    {
+        die('Impossible de se connecter à la base parce que : ' . $e->getMessage());
+    }
+
+?>
